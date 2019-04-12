329ec37cde38cbbc7677851d07c1c5132f73be35
Magdalena Wrona <wrona_m@wp.pl>
2019-04-12 09:20:43 +0200

    change weather api

--- main.js
+++ main.js
@@ -13,26 +13,36 @@ window.addEventListener("load", () => {
       szer = position.coords.latitude;
       // console.log(dl, szer);
       const proxy = "https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}https://api.darksky.net/forecast/12c100ebcafecc60acd8525e7f9e04a6/${szer},${dl}`;
+      // const api = `${proxy}https://api.darksky.net/forecast/12c100ebcafecc60acd8525e7f9e04a6/${szer},${dl}`;
 
+
+
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${szer}&lon=${dl}&APPID=b25fd1e90c25991bbe5d57ab854aaaab`
+
+
+      // console.log(api);
 
       fetch(api)
         .then(response => {
           return response.json();
         })
         .then(data => {
-          // console.log(data);
-          const place = data.timezone;
+          console.log(data);
+          const place = data.name;
           // console.log(place);
-          placeHtml.textContent = place;
-          const degree = data.currently.temperature;
-          const celsius = Math.floor((degree - 32) * (10 / 18));
+          const country = data.sys.country;
+          placeHtml.textContent = `${place}, ${country}`;
+          const degree = data.main.temp;
+          // const celsius = Math.floor((degree - 32) * (10 / 18));
+          const celsius = Math.floor(degree - 273.15);
           degreeHtml.innerHTML = `${celsius}&degC`;
-          const opis = data.currently.summary;
+          // degreeHtml.textContent = degree;
+          const opis = data.weather[1].description;
+          // console.log(opis);
           opisHtml.textContent = opis;
 
-          icon = data.currently.icon;
-          setIcons(icon, document.querySelector('.icon'));
+          // icon = data.currently.icon;
+          // setIcons(icon, document.querySelector('.icon'));
         });
 
     });
