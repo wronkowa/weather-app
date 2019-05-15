39b8683cbeee4fd49d0d3e28820305c3ae7b2ae0
Magdalena Wrona <wrona_m@wp.pl>
2019-05-15 18:36:30 +0200

    add current date

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const placeName = document.querySelector('.place-name');
 let dl = 0;
 let szer = 0;
+let nameDay = "";
+
+function setNameDay(dayNumber) {
+  switch (dayNumber) {
+    case 0: nameDay = "Sunday"; break;
+    case 1: nameDay = "Monday"; break;
+    case 2: nameDay = "Tuesday"; break;
+    case 3: nameDay = "Wednesday"; break;
+    case 4: nameDay = "Thursday"; break;
+    case 5: nameDay = "Friday"; break;
+    case 5: nameDay = "Saturday"; break;
+  }
+}
+
+function getDate() {
+  const currentDate = new Date();
+  let dayNumber = currentDate.getDay();
+  setNameDay(dayNumber);
+  // console.log(nameDay);
+  document.querySelector('.day').textContent = nameDay;
+  const day = currentDate.getDate();
+  let month = currentDate.getMonth() + 1;
+  if (month < 10) {
+    month = `0${month}`;
+    // console.log(month);
+  }
+  const year = currentDate.getFullYear();
+
+  document.querySelector('.date').textContent = `${day}.${month}.${year}`;
+}
 
 function getPosition() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
+      // console.log(position);
       dl = position.coords.longitude;
       szer = position.coords.latitude;
     });
@@ -21,7 +51,10 @@ function getWeather(dl, szer) {
       return response.json();
     })
     .then(data => {
-      console.log(data);
+      // console.log(data);
+      const cityName = data.name;
+      // console.log(cityName);
+      document.querySelector('.place-name').value = cityName;
       const degreeKelvin = data.main.temp;
       const degreeCelsius = degreeKelvin - (273.15);
       // console.log(degreeCelsius);
@@ -39,6 +72,7 @@ function getWeather(dl, szer) {
 
 }
 
-
+getDate();
 getPosition();
+// getWeather(21.0117800, 52.2297700);
 getWeather(dl, szer);
