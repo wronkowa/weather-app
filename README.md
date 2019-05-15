5d863c46bc82f4c870f2cbea1582335bd072c409
Magdalena Wrona <wrona_m@wp.pl>
2019-05-15 12:45:59 +0200

    reorganization app, add readme, change css, without media queries, only mobile, without js

---
+++ README.md
@@ -0,0 +1,7 @@
+#WEATHER APP
+
+##Technologies
+Project is created with:
+* HTMl 5
+* CSS 3
+* JavaScript
--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const userCity = document.querySelector("#city");
-const btnCheckWeather = document.querySelector("button");
-const placeHtml = document.querySelector('.place');
-const degreeHtml = document.querySelector('.degree');
-const opisHtml = document.querySelector('p');
+// const userCity = document.querySelector("#city");
+// const btnCheckWeather = document.querySelector("button");
+// const placeHtml = document.querySelector('.place-name');
+// const degreeHtml = document.querySelector('.degree');
+// const opisHtml = document.querySelector('p');
 
-btnCheckWeather.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (userCity.value === "") return alert("Wpisz nazwe miejsca");
-  else {
-    // console.log(userCity.value);
-    apiUser = `https://api.openweathermap.org/data/2.5/weather?q=${userCity.value}&APPID=b25fd1e90c25991bbe5d57ab854aaaab`;
-    document.querySelector('.result').classList.add('active');
-    fetch(apiUser)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        // console.log(data);
-        const place = userCity.value;
-        const country = data.sys.country;
-        placeHtml.innerHTML = `Pogoda dla: <span>${place.toUpperCase()}, ${country}</span>`;
-        const degree = data.main.temp;
-        const celsius = Math.floor(degree - 273.15);
-        degreeHtml.innerHTML = `Temperatura: <span>${celsius}&degC</span>`;
-        const opis = data.weather[0].description;
-        opisHtml.textContent = opis;
-      });
-  }
-})
+// btnCheckWeather.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (userCity.value === "") return alert("Wpisz nazwe miejsca");
+//   else {
+//     // console.log(userCity.value);
+//     apiUser = `https://api.openweathermap.org/data/2.5/weather?q=${userCity.value}&APPID=b25fd1e90c25991bbe5d57ab854aaaab`;
+//     document.querySelector('.result').classList.add('active');
+//     fetch(apiUser)
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(data => {
+//         // console.log(data);
+//         const place = userCity.value;
+//         const country = data.sys.country;
+//         placeHtml.innerHTML = `Pogoda dla: <span>${place.toUpperCase()}, ${country}</span>`;
+//         const degree = data.main.temp;
+//         const celsius = Math.floor(degree - 273.15);
+//         degreeHtml.innerHTML = `Temperatura: <span>${celsius}&degC</span>`;
+//         const opis = data.weather[0].description;
+//         opisHtml.textContent = opis;
+//       });
+//   }
+// })
 
 
 /* window.addEventListener("load", () => {
